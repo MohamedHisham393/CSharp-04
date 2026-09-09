1953c460c53db6eb603dff9cdd46393fd762982c
MohamedHisham393 <mohamed.hisham12393@gmail.com>
2026-09-09 10:22:23 +0300

    1st question

--- C#-04 Assignment/Program.cs
+++ C#-04 Assignment/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Q1
+            int pages = 464;
+            bool isAvailable = true;
+
+            if (pages > 300 && isAvailable == true)
+            {
+                Console.WriteLine("You can borrow this book");
+            }
+
+            //-------------------------------------------------------
         }
     }
 }
