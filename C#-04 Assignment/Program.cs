bbe3ca6b7598e9fe49a0cfea34067f01f702a016
MohamedHisham393 <mohamed.hisham12393@gmail.com>
2026-09-09 10:41:10 +0300

    4th question

--- C#-04 Assignment/Program.cs
+++ C#-04 Assignment/Program.cs
@@ -16,29 +16,40 @@
             //-------------------------------------------------------
 
             //Q2
-            string title = "Refactoring";
+            //string title = "Refactoring";
 
-            switch (title)
-            {
-                case "Clean Code":
-                    Console.WriteLine("Great choice!");
-                    break;
-                case "Refactoring":
-                    Console.WriteLine("Nice pick!");
-                    break;
-                default:
-                    Console.WriteLine("Never heard of it");
-                    break;
-            }
+            //switch (title)
+            //{
+            //    case "Clean Code":
+            //        Console.WriteLine("Great choice!");
+            //        break;
+            //    case "Refactoring":
+            //        Console.WriteLine("Nice pick!");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Never heard of it");
+            //        break;
+            //}
 
             //--------------------------------------------------------
 
             //Q3
 
-            int pages = 464;
-            
-            string sizeLabel = (pages > 300) ? "Long Book" :"Short Book";
-            Console.WriteLine(sizeLabel);
+            //int pages = 464;
+
+            //string sizeLabel = (pages > 300) ? "Long Book" :"Short Book";
+            //Console.WriteLine(sizeLabel);
+
+            //--------------------------------------------------------
+
+            //Q4
+
+            string[] books = { "Clean Code", "The Pragmatic Programmer", "Refactoring" };
+
+            for (int i = 0; i< books.Length; i++)
+            {
+                Console.WriteLine($"Book number {i+1} is {books[i]}" );
+            }
         }
     }
 }
