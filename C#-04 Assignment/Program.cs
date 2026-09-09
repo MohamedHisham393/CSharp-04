02077b1f1b7afd8a59b0e7ad939effc0250d12f3
MohamedHisham393 <mohamed.hisham12393@gmail.com>
2026-09-09 10:23:58 +0300

    2nd question

--- C#-04 Assignment/Program.cs
+++ C#-04 Assignment/Program.cs
@@ -5,15 +5,34 @@
         static void Main(string[] args)
         {
             //Q1
-            int pages = 464;
-            bool isAvailable = true;
+            //int pages = 464;
+            //bool isAvailable = true;
 
-            if (pages > 300 && isAvailable == true)
-            {
-                Console.WriteLine("You can borrow this book");
-            }
+            //if (pages > 300 && isAvailable == true)
+            //{
+            //    Console.WriteLine("You can borrow this book");
+            //}
 
             //-------------------------------------------------------
+
+            //Q2
+            string title = "Refactoring";
+
+            switch (title)
+            {
+                case "Clean Code":
+                    Console.WriteLine("Great choice!");
+                    break;
+                case "Refactoring":
+                    Console.WriteLine("Nice pick!");
+                    break;
+                default:
+                    Console.WriteLine("Never heard of it");
+                    break;
+            }
+
+            //--------------------------------------------------------
+
         }
     }
 }
